d5530127a4781e9fb18ca0da1478f6515a4d2a2c
sewhitebook <sewhitebook@ucsb.edu>
2024-03-25 14:30:41 -0700

    Solved inconsistency

--- src/fpw.h
+++ src/fpw.h
@@ -11,5 +11,6 @@ using std::vector;
 VectorXi makeIndices(const VectorXd& times, const double f, const int N_bins);
 double deltaChi2(const VectorXd& y, const VectorXd& ivar, const VectorXi& ind, int N_bins, int N_dat);
 vector<double> runFPW(const vector<double>& t, const vector<double>& y, const vector<double>& ivar, const vector<double>& freqs, int N_bins);
+vector<double> testFPW(const vector<double>& t, const vector<double>& y, const vector<double>& ivar, const vector<double>& freqs, int N_bins);
 
 #endif // FPW_H
