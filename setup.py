e4c64e869fab8b3eebde142169c84bffa43fc263
sewhitebook <sewhitebook@ucsb.edu>
2024-03-28 14:50:07 -0700

    modifying setup.py for MacOS

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy
+import sys
+
+extra_compile_args = ["-O3", "-DNDEBUG"]
+if sys.platform == "darwin":  # macOS
+    extra_compile_args += ["-std=c++11", "-Wno-nullability-completeness"]
 
 extensions = [
     Extension(
@@ -9,7 +14,7 @@ extensions = [
         ["fpw.pyx", "src/fpw.cpp"],  # Keep the relative path to fpw.cpp
         include_dirs=[numpy.get_include(), 'include/eigen', 'src'],
         language="c++",
-        extra_compile_args=["-O3", "-DNDEBUG"],
+        extra_compile_args=extra_compile_args,
     )
 ]
 
