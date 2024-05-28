303e9bb83fd3a31d66e7ef88f77204b43aac8a67
sewhitebook <sewhitebook@ucsb.edu>
2024-05-28 10:46:19 -0700

    Fixed function definition

--- src/fpw.h
+++ src/fpw.h
@@ -6,7 +6,7 @@ using std::vector;
 
 int* makeIndices(double* times, const double f, const int N_bins, const int size);
 double deltaChi2(double* y, double* ivar, int* ind, int N_bins, int N_dat);
-vector<double> runFPW(const vector<double>& t, const vector<double>& y, const vector<double>& dy, const vector<double>& freqs, int N_bins);
+vector<double> runFPW(vector<double>& t, vector<double>& y, vector<double>& dy, const vector<double>& freqs, int N_bins);
 // vector<double> testFPW(const vector<double>& t, const vector<double>& y, const vector<double>& ivar, const vector<double>& freqs, int N_bins);
 
 #endif // FPW_H
