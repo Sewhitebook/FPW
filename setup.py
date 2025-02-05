dea419503ab54c786ff8810c2a8c5cb185b1a8b6
Samuel E Whitebook <sewhitebook@astro.caltech.edu>
2025-02-05 15:52:33 -0800

    Update setup.py to Include Version Number

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ extensions = [
     Extension(
         "fpw",
         ["fpw.pyx", "src/fpw.cpp"],  # Keep the relative path to fpw.cpp
-        # include_dirs=[numpy.get_include(), 'include/eigen', 'src'],
         include_dirs=[numpy.get_include(), 'src'],
         language="c++",
         extra_compile_args=extra_compile_args,
@@ -19,6 +18,7 @@ extensions = [
 
 setup(
     name="fpw",
+    version="1.0.1",
     ext_modules=cythonize(extensions),
     install_requires=[
         'numpy',
